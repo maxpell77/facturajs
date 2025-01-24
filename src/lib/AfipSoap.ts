06754c20c09fb5cfee68090b31da6b6f76c29cec
Maximiliano Pell <maximilianopell@192.168.1.10>
2025-01-24 20:13:50 -0300

    correccion .env

--- src/lib/AfipSoap.ts
+++ src/lib/AfipSoap.ts
@@ -4,7 +4,7 @@ import { IConfigService } from '../IConfigService';
 import { WsServicesNames } from '../SoapMethods';
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 const s3Client = new S3Client({
-    region: process.env.AWS_REGION, // Asegúrate de tener configurada la región
+    region: process.env.AWS_S3_REGION, // Asegúrate de tener configurada la región
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
